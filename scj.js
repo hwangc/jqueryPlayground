11dda745b582a0bf1a05fe2a47dbdc17885f3c42
hwangc <hwangc@hwangc.com>
2017-07-17 10:23:13 +0900

    Select comments from DOM and manipulate them
    h: from the contents retrieved by jquery search only nodeType == 8 which means comment
    y: need to add an attribute after a certain comment

---
+++ scj.js
@@ -0,0 +1,8 @@
+/* to add an attribute to the first element after the xxx comment  */
+$('body').contents().each(function() {
+	// nodeType of comment is 8 and its value is nodeValue
+	if(this.nodeType == 8 && this.nodeValue == 'xxx') {
+		$(this).nextAll().eq(0).attr({'id':'yyy'});
+		return false;
+	}
+});
