9641a341ca03e029cf0f8bce3958421643789e6c
hwangc <hwangc@hwangc.com>
2017-07-17 10:27:28 +0900

    Add comment for the reason of return false
    h: return false;
    y: doesn't need to run the loop any more once I find the comment

--- scj.js
+++ scj.js
@@ -3,6 +3,7 @@ $('body').contents().each(function() {
 	// nodeType of comment is 8 and its value is nodeValue
 	if(this.nodeType == 8 && this.nodeValue == 'xxx') {
 		$(this).nextAll().eq(0).attr({'id':'yyy'});
+		// false return can break the each loop
 		return false;
 	}
 });
